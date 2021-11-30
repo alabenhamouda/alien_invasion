e1f068bac62d7e155d48756fbb03dfa6298e14bd
alabh <alabenhmouda@gmail.com>
2021-11-30 19:03:37 +0100

    restore bullet width

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings():
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 1.5
         self.bullet_speed_factor = 1
-        self.bullet_width = 1200
+        self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 5
